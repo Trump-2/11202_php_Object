f1e906668fd80d004a8ef1a54683eea5022f4247
YIHONG <akashidaiki2@gmail.com>
2023-11-24 10:38:30 +0800

    微調整建構式

--- index.php
+++ index.php
@@ -7,6 +7,14 @@ class Animal
 
   protected $name;
 
+  // 建構式；物件被 new 出來的當下會被執行
+  public function __construct($name)
+  {
+    // echo "hi";
+    $this->name = $name;
+  }
+
+
   public function setName($name)
   {
     // 存取物件內的屬性的寫法，注意屬性前面沒有 $
@@ -19,13 +27,13 @@ class Animal
   }
 }
 
-$animal = new Animal; // 實體化
+$animal = new Animal('羅賓'); // 實體化
 
 echo "顯示名稱:" . $animal->getName();
 echo "<br>";
 $animal->setName('魯蛇');
 echo "顯示名稱:" . $animal->getName();
 echo "<br>";
-$animal->name = '阿忠';
-echo "顯示名稱:" . $animal->name;
+// $animal->name = '阿忠';
+// echo "顯示名稱:" . $animal->name;
 echo "<br>";
