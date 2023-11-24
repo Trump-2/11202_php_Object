6e853294eafddff3f7b7f849b5bcadbbb2584699
YIHONG <akashidaiki2@gmail.com>
2023-11-24 16:10:18 +0800

    修改update的參數以縮減程式碼以及增加q()及count()函式

--- db.php
+++ db.php
@@ -45,6 +45,35 @@ class DB
     }
   }
 
+  // 聚合函數 「count」的專用函數
+  function count($where = '', $other = '')
+  {
+    $sql = "select count(*) from `$this->table` ";
+
+    if (isset($this->table) && !empty($this->table)) {
+
+      if (is_array($where)) {
+
+        if (!empty($where)) {
+          foreach ($where as $col => $value) {
+            $tmp[] = "`$col`='$value'";
+          }
+          $sql .= " where " . join(" && ", $tmp);
+        }
+      } else {
+        $sql .= " $where";
+      }
+
+      $sql .= $other;
+      //echo 'all=>'.$sql;
+      $rows = $this->pdo->query($sql)->fetchColumn();
+      return $rows;
+    } else {
+      echo "錯誤:沒有指定的資料表名稱";
+    }
+  }
+
+
   function total($id)
   {
     $sql = "select count(`id`) from `$this->table` ";
@@ -87,54 +116,92 @@ class DB
   function save($array)
   {
     if (isset($array['id'])) {
-      $this->update($array['id'], $array);
-    } else {
-      $this->insert($array);
-    }
-  }
+      $sql = "update `$this->table` set ";
 
-  protected function update($id, $cols)
-  {
-
-    $sql = "update `$this->table` set ";
-
-    if (!empty($cols)) {
-      foreach ($cols as $col => $value) {
-        $tmp[] = "`$col`='$value'";
+      if (!empty($array)) {
+        foreach ($array as $col => $value) {
+          $tmp[] = "`$col`='$value'";
+        }
+      } else {
+        echo "錯誤:缺少要編輯的欄位陣列";
       }
-    } else {
-      echo "錯誤:缺少要編輯的欄位陣列";
-    }
 
-    $sql .= join(",", $tmp);
-    $tmp = [];
-    if (is_array($id)) {
-      foreach ($id as $col => $value) {
-        $tmp[] = "`$col`='$value'";
-      }
-      $sql .= " where " . join(" && ", $tmp);
-    } else if (is_numeric($id)) {
-      $sql .= " where `id`='$id'";
+      $sql .= join(",", $tmp) . " where `id`='{$array['id']}'";
     } else {
-      echo "錯誤:參數的資料型態比須是數字或陣列";
+      $sql = "insert into `$this->table` ";
+      $cols = "(`" . join("`,`", array_keys($array)) . "`)";
+      $vals = "('" . join("','", $array) . "')";
+
+      $sql = $sql . $cols . " values " . $vals;
     }
-    // echo $sql;
     return $this->pdo->exec($sql);
   }
 
-  protected function insert($values)
-  {
+  // protected function update($id, $cols)
+  // {
 
-    $sql = "insert into `$this->table` ";
-    $cols = "(`" . join("`,`", array_keys($values)) . "`)";
-    $vals = "('" . join("','", $values) . "')";
+  //   $sql = "update `$this->table` set ";
 
-    $sql = $sql . $cols . " values " . $vals;
+  //   if (!empty($cols)) {
+  //     foreach ($cols as $col => $value) {
+  //       $tmp[] = "`$col`='$value'";
+  //     }
+  //   } else {
+  //     echo "錯誤:缺少要編輯的欄位陣列";
+  //   }
 
-    //echo $sql;
+  //   $sql .= join(",", $tmp);
+  //   $tmp = [];
+  //   if (is_array($id)) {
+  //     foreach ($id as $col => $value) {
+  //       $tmp[] = "`$col`='$value'";
+  //     }
+  //     $sql .= " where " . join(" && ", $tmp);
+  //   } else if (is_numeric($id)) {
+  //     $sql .= " where `id`='$id'";
+  //   } else {
+  //     echo "錯誤:參數的資料型態比須是數字或陣列";
+  //   }
+  //   // echo $sql;
+  //   return $this->pdo->exec($sql);
+  // }
 
-    return $this->pdo->exec($sql);
-  }
+
+  // 確定 $id 的值就是數字的 function 寫法
+  // protected function update($cols)
+  // {
+
+  //   $sql = "update `$this->table` set ";
+
+  //   if (!empty($cols)) {
+  //     foreach ($cols as $col => $value) {
+  //       $tmp[] = "`$col`='$value'";
+  //     }
+  //   } else {
+  //     echo "錯誤:缺少要編輯的欄位陣列";
+  //   }
+
+  //   $sql .= join(",", $tmp) . " where `id`='{$cols['id']}'";
+  //   // echo $sql;
+  //   return $this->pdo->exec($sql);
+  // }
+
+
+
+
+  // protected function insert($values)
+  // {
+
+  //   $sql = "insert into `$this->table` ";
+  //   $cols = "(`" . join("`,`", array_keys($values)) . "`)";
+  //   $vals = "('" . join("','", $values) . "')";
+
+  //   $sql = $sql . $cols . " values " . $vals;
+
+  //   //echo $sql;
+
+  //   return $this->pdo->exec($sql);
+  // }
 
   function del($id)
   {
@@ -153,6 +220,13 @@ class DB
     //echo $sql;
 
     return $this->pdo->exec($sql);
+  }
+
+
+  // pdo->query() 專用的函數
+  function q($sql)
+  {
+    return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
   }
 }
 
