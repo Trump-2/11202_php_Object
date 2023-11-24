b22df4e4e01278e00f71f92fdbbc4e128112be0f
YIHONG <akashidaiki2@gmail.com>
2023-11-24 10:37:55 +0800

    類別封裝權限

--- index.php
+++ index.php
@@ -3,17 +3,29 @@
 
 class Animal
 {
-  public $name;
+
+
+  protected $name;
 
   public function setName($name)
   {
+    // 存取物件內的屬性的寫法，注意屬性前面沒有 $
     $this->name = $name;
+  }
+
+  public function getName()
+  {
+    return $this->name;
   }
 }
 
 $animal = new Animal; // 實體化
 
-echo "顯示名稱:" . $animal->name;
+echo "顯示名稱:" . $animal->getName();
 echo "<br>";
 $animal->setName('魯蛇');
+echo "顯示名稱:" . $animal->getName();
+echo "<br>";
+$animal->name = '阿忠';
 echo "顯示名稱:" . $animal->name;
+echo "<br>";
