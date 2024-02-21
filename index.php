f87a183ca62ae72b24c3039e0dcbcb0094961094
Trump-2 <akashidaiki2@gmail.com>
2024-02-21 21:42:21 +0800

    添加測試用 index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+include "./02_21-2.php";
+$User->save(['id' => 3, 'acc' => 'Lebron', 'pw' => 666, 'email' => "test4@gmail.com"]);
+// $User->save(['acc' => "Kobe", 'pw' => 7777, "email" => 'test2@gmail.com']);
